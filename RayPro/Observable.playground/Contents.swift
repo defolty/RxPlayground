768883585ad1d66b8628db875064052b8e0d4b58
nikitanesporov <nikitanesporov@gmail.com>
2022-08-09 23:46:31 +0500

    Complete 104

--- RayPro/Observable.playground/Contents.swift
+++ RayPro/Observable.playground/Contents.swift
@@ -1,12 +1,35 @@
 import Foundation
 import RxSwift
 
+public let episodeI = "The Phantom Menace"
+public let episodeII = "Attack of the Clones"
+public let theCloneWars = "The Clone Wars"
+public let episodeIII = "Revenge of the Sith"
+public let solo = "Solo"
+public let rogueOne = "Rogue One"
+public let episodeIV = "A New Hope"
+public let episodeV = "The Empire Strikes Back"
+public let episodeVI = "Return Of The Jedi"
+public let episodeVII = "The Force Awakens"
+public let episodeVIII = "The Last Jedi"
+public let episodeIX = "Episode IX"
+
 public func example(of description: String, action: () -> Void) {
     print("\n--- Example of:", description, "---")
     action()
 }
- 
-
+  
 example(of: "creating observable") {
-    <#code#>
+    // .just - всё что делает just это создает наблюдаемую последовательность содержащую только один элемент
+    // .just - по идеи это метод, но в RxSwift это назывыается оператор
+    let mostPopular: Observable<String> = Observable<String>.just(episodeV)
+    
+    // это наблюдатель типа String
+    let originalTrilogy = Observable.of(episodeIV, episodeV, episodeVI)
+    
+    // это наблюдаемый массив строк
+    let prequelTrilogy = Observable.of([episodeI, episodeII, episodeIII])
+    
+    // это наблюдатель типа String
+    let sequelTrilogy = Observable.from([episodeVII, episodeVIII, episodeIX])
 }
